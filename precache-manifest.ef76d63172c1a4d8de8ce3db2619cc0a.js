fb5083f0721a29b4f05f431a361d5a17dc7de7a9
nebojsaj1726 <nebojsa.jacovic@gmail.com>
2020-05-11 20:31:19 +0200

    Updates

---
+++ precache-manifest.ef76d63172c1a4d8de8ce3db2619cc0a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c2b897a19236f6906337b71d501b3fe",
+    "url": "/fe-wm/index.html"
+  },
+  {
+    "revision": "22757ad3337a00fe6547",
+    "url": "/fe-wm/static/css/main.6a3de1d2.chunk.css"
+  },
+  {
+    "revision": "9f565c03218a8e5bc17d",
+    "url": "/fe-wm/static/js/2.65d042ad.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/fe-wm/static/js/2.65d042ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22757ad3337a00fe6547",
+    "url": "/fe-wm/static/js/main.f1ff1c22.chunk.js"
+  },
+  {
+    "revision": "d4c71eabfa8f6e9e6363",
+    "url": "/fe-wm/static/js/runtime-main.bdd139e0.js"
+  }
+]);
